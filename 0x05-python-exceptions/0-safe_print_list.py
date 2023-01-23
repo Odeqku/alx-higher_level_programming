45364d60fb55a473e75c7705c50b6c96627e7348
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2023-01-23 21:32:10 +0000

    0-safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        y = 0 
+        y = 0
         for elem in my_list:
             if x:
                 x -= 1
                 y += 1
-                print(elem, end='')    
+                print(elem, end='')
         print()
 
     except Exception as ex:
