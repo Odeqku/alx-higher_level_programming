a28502e2dc64422c188a0eb0455af0910da368be
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2023-01-24 17:31:02 +0000

    Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    import sys
+    var = False
+    var1 = True
+    try:
+        print("{:d}".format(value))
+    except ValueError as ex:
+        print("Exception: ", ex, file=sys.stderr)
+        return False
+    else:
+        return True
+        
