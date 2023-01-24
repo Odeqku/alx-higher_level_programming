cbed5ac0e2d0539ff411438751ed6304c4530f10
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2023-01-24 18:35:27 +0000

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        a, b = args
+        return fct(a, b)
+    except Exception as ex:
+        sys.stderr.write("Exception: {}\n".format(ex))
+        return None
