3a24defdb9a177a530ee0122827350af232108ad
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2023-01-24 01:56:04 +0000

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except Exception:
+    except (ZeroDevisionError, TypoError):
         pass
     finally:
-        print("Inside result: {}".format(result if b > 0 else 'None'))
-        return (result if b > 0 else 'None')
+        print("Inside result: {}".format(result if b > 0 else None))
+        return (result if b > 0 else None)
