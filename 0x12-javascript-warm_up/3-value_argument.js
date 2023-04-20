3eb1568ff34cba0f7be87398d5b91e33ae4f8112
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2023-04-20 23:30:49 +0000

    JavaScript is amazing

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const myVar1 = process.argv.length - 2;
+// const myVar1 = process.argv.length - 2;
 const myVar2 = process.argv;
-if (myVar1 === 0) {
+if (!myVar2[2]) {
   console.log('No argument');
 } else {
   console.log(myVar2[2]);
