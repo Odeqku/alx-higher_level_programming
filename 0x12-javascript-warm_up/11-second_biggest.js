db03b5f49cf7b49acf44c5e073c4605182521c6d
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2023-04-24 20:16:32 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,22 +4,19 @@ const myArr = process.argv;
 
 const myBiggest = biggest(myArr);
 
-const new_Arr = bigger(myArr, myBiggest);
+const newArr = bigger(myArr, myBiggest);
 
-console.log(biggest(new_Arr));
+console.log(biggest(newArr));
 
-function biggest(arr) {
+function biggest (arr) {
   const zero = 0;
   const myVar = arr;
   let biggest = Number(myVar[2]);
   let n = 0;
 
-
   if (typeof myVar[2] === 'undefined' && isNaN(Number(myVar[0]))) {
     return zero;
-  }
-
-  else if (isNaN(Number(myVar[0])) && myVar.length > 3) {
+  } else if (isNaN(Number(myVar[0])) && myVar.length > 3) {
     n = myVar.length - 2;
 
     for (let i = 1; i <= n; i++) {
@@ -29,27 +26,27 @@ function biggest(arr) {
     }
     return biggest;
   } else if (myVar.length === 1) {
-      return zero;
-	  } else {
-	      let biggest1 = Number(myVar[0]);
-          for (let i = 1; i <= myVar.length; i++) {
-            if (biggest1 < Number(myVar[i])) {
-              biggest1 = myVar[i];
-		    }
-	      }
-	      return biggest1;
+    return zero;
+  } else {
+    let biggest1 = Number(myVar[0]);
+    for (let i = 1; i <= myVar.length; i++) {
+      if (biggest1 < Number(myVar[i])) {
+        biggest1 = myVar[i];
       }
+    }
+    return biggest1;
+  }
 }
 
-function bigger(myArr, biggest) {
-  let new_arr = [];
+function bigger (myArr, biggest) {
+  const newArr = [];
   const biggest2 = Number(biggest);
   const n = myArr.length - 2;
 
   for (let i = 2; i <= n + 1; i++) {
     if (!(biggest2 === Number(myArr[i]))) {
-      new_arr.push(Number(myArr[i]));
+      newArr.push(Number(myArr[i]));
     }
   }
-  return new_arr;
+  return newArr;
 }
